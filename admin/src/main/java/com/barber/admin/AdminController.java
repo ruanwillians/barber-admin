b0bc9c09a4f9b90b9d7f685b1268ae4608be0e2e
Ruan Willians Mira Nogueira <108192932+ruanwillians@users.noreply.github.com>
2024-12-17 22:09:59 -0300

    Fix: health api route

--- admin/src/main/java/com/barber/admin/AdminController.java
+++ admin/src/main/java/com/barber/admin/AdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
-    @GetMapping("/teste")
+    @GetMapping("/")
     public String getAdminPage() {
         return "health api";
     }
